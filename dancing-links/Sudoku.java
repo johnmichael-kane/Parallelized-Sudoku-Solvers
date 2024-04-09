97fa33d64bc075441f1f254623a1e7b350c82718
CandyKaine1 <thejohnmichaelkane@gmail.com>
2024-04-09 19:18:10 -0400

    removed unused 'include'

--- dancing-links/Sudoku.java
+++ dancing-links/Sudoku.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
