6e664a03e68542e0eb1763b25a720f3f6e51275c
CandyKaine1 <thejohnmichaelkane@gmail.com>
2024-04-09 23:19:12 -0400

    Seperating the Non-Parallelized Solution

---
+++ dancing-links/basic/Cell.java
@@ -0,0 +1,12 @@
+public class Cell {
+
+	public int row;
+	public int col;
+	public int number;
+	
+	public Cell(int row, int col, int number) {
+		this.row=row;
+		this.col=col;
+		this.number=number;
+	}
+}
