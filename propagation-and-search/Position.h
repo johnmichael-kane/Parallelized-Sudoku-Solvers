ca9a65dfb8d58af524fa3d7bd8dd6ab90fe8a292
sol <soleilcordray@gmail.com>
2024-04-14 15:07:40 -0400

    replace Pos with Position

---
+++ propagation-and-search/Position.h
@@ -0,0 +1,26 @@
+// Name        : Grid.cpp
+// Author      : Hongbo Tian
+// Editor      : Soleil Cordray
+
+#ifndef POSITION_H_
+#define POSITION_H_
+
+#include <sstream>
+
+using namespace std;
+
+struct Position
+{
+    int row, col;
+
+    Position(int r, int c) : row(r), col(c) {}
+
+    string toString() const
+    {
+        stringstream ss;
+        ss << "[" << row << ", " << col << "]";
+        return ss.str();
+    }
+};
+
+#endif /* POSITION_H_ */
