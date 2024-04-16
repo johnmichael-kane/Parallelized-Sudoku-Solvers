674487bb1b39c791e533f427fea927857cd42738
JohnMichael Kane <123655765+johnmichael-kane@users.noreply.github.com>
2024-04-16 10:20:31 -0400

    printing and shit

--- dancing-links/Sudoku.java
+++ dancing-links/Sudoku.java
@@ -20,19 +20,21 @@ class ExactMatrix {
 
 	public int[][] matrix;
 	int sudokuSize;
-	
+
 	public ExactMatrix(int[][] matrix) {
 		this.matrix = matrix;
 		sudokuSize = this.matrix.length;
-		
+
 	}
-	
-	//rather than creating finalMatrix, initMatrix, then makeFinalMatrix, we keep it all for that function
+
+	// rather than creating finalMatrix, initMatrix, then makeFinalMatrix, we keep
+	// it all for that function
 	public Cell[][] makeFinalMatrix() {
-		Cell[][] finalMatrix = new Cell[matrix.length*matrix.length*matrix.length][matrix[0].length*matrix[0].length*4];
+		Cell[][] finalMatrix = new Cell[matrix.length * matrix.length * matrix.length][matrix[0].length
+				* matrix[0].length * 4];
 		initMatrix(finalMatrix);
-		for (int i=0; i<sudokuSize; i++) {
-			for (int j=0; j<sudokuSize; j++){
+		for (int i = 0; i < sudokuSize; i++) {
+			for (int j = 0; j < sudokuSize; j++) {
 				fillInMatrix(finalMatrix, i, j, matrix[i][j]);
 			}
 		}
@@ -40,12 +42,12 @@ class ExactMatrix {
 	}
 
 	private void initMatrix(Cell[][] finalMatrix) {
-		for (int i=0;i<finalMatrix.length; i++) {
-			for (int j=0;j<finalMatrix[0].length; j++){
+		for (int i = 0; i < finalMatrix.length; i++) {
+			for (int j = 0; j < finalMatrix[0].length; j++) {
 				finalMatrix[i][j] = null;
 			}
 		}
-		
+
 	}
 
 	private void fillInMatrix(Cell[][] finalMatrix, int row, int col, int number) {
@@ -55,13 +57,13 @@ class ExactMatrix {
 			}
 		}
 	}
-	
+
 	private void fillCell(Cell[][] finalMatrix, int row, int col, int number) {
-		int matrixRow = row*matrix.length*matrix.length + col*matrix.length+number-1;
-		int col1 = row*sudokuSize+col;
-		int col2 = sudokuSize*sudokuSize+row*sudokuSize+number-1;
-		int col3 = 2*sudokuSize*sudokuSize+col*sudokuSize+number-1;
-		int col4 = 3*sudokuSize*sudokuSize+getGridNumber(row, col)*sudokuSize+number-1;
+		int matrixRow = row * matrix.length * matrix.length + col * matrix.length + number - 1;
+		int col1 = row * sudokuSize + col;
+		int col2 = sudokuSize * sudokuSize + row * sudokuSize + number - 1;
+		int col3 = 2 * sudokuSize * sudokuSize + col * sudokuSize + number - 1;
+		int col4 = 3 * sudokuSize * sudokuSize + getGridNumber(row, col) * sudokuSize + number - 1;
 		finalMatrix[matrixRow][col1] = new Cell(row, col, number);
 		finalMatrix[matrixRow][col2] = new Cell(row, col, number);
 		finalMatrix[matrixRow][col3] = new Cell(row, col, number);
@@ -69,8 +71,8 @@ class ExactMatrix {
 	}
 
 	private int getGridNumber(int row, int col) {
-		int gridSize = (int)Math.sqrt(sudokuSize);
-		return (col/gridSize)+((int)(row/gridSize))*gridSize;
+		int gridSize = (int) Math.sqrt(sudokuSize);
+		return (col / gridSize) + ((int) (row / gridSize)) * gridSize;
 	}
 }
 
@@ -78,35 +80,34 @@ public class Sudoku {
 
 	private static int boardSize = 0;
 	private static int partitionSize = 0;
-	private static long startTime; 
-	private static long endTime; 
+	private static long startTime;
+	private static long endTime;
 	private static final AtomicReference<int[][]> solutionMatrix = new AtomicReference<>();
 	public static int BFS_DEPTH;
-	
 
 	// Method to format single-digit numbers with leading zero
 	public static String n(int n) {
 		return n > 9 ? "" + n : "0" + n;
 	}
 
-	//used for BFS, this just checks whether or not a move can work 
-	//there should be nothing wrong with this part
+	// used for BFS, this just checks whether or not a move can work
+	// there should be nothing wrong with this part
 	private static boolean isValidMove(int[][] board, Point cell, int num) {
 		int row = cell.x;
 		int col = cell.y;
-		
+
 		// Check row and column
 		for (int i = 0; i < board.length; i++) {
 			if (board[row][i] == num || board[i][col] == num) {
 				return false;
 			}
 		}
-	
+
 		// Check block
-		int sqrt = (int)Math.sqrt(board.length);
+		int sqrt = (int) Math.sqrt(board.length);
 		int blockRowStart = row - row % sqrt;
 		int blockColStart = col - col % sqrt;
-	
+
 		for (int r = blockRowStart; r < blockRowStart + sqrt; r++) {
 			for (int c = blockColStart; c < blockColStart + sqrt; c++) {
 				if (board[r][c] == num) {
@@ -114,11 +115,11 @@ public class Sudoku {
 				}
 			}
 		}
-	
+
 		return true; // No violations, so it's a valid move
 	}
-	
-	//used for BFS
+
+	// used for BFS
 	private static Point findNextEmptyCell(int[][] board) {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
@@ -130,7 +131,7 @@ public class Sudoku {
 		return null; // No empty cells found
 	}
 
-	//used for BFS
+	// used for BFS
 	private static int calculateEmptyCells(int[][] board) {
 		int emptyCells = 0;
 		for (int[] row : board) {
@@ -144,19 +145,18 @@ public class Sudoku {
 		return emptyCells;
 	}
 
-	//this is the function that does the initial BFS and splits them up
+	// this is the function that does the initial BFS and splits them up
 	private static List<int[][]> generateStartingBoards(int[][] originalBoard, int blockSize, int numBoards) {
 		List<int[][]> boards = new ArrayList<int[][]>(); // Specify the type inside <>
-		if(numBoards == 1){
+		if (numBoards == 1) {
 			boards.add(deepCopy(originalBoard));
-		}
-		else {
-			BFS_DEPTH=calculateEmptyCells(originalBoard) - 3;
+		} else {
+			BFS_DEPTH = calculateEmptyCells(originalBoard) - 3;
 
 			// BFS Queue to hold the next cells to try
 			Queue<int[][]> queue = new LinkedList<>();
 			queue.add(originalBoard);
-		
+
 			// Start BFS until you have enough starting boards or the queue is empty
 			while (!queue.isEmpty() && boards.size() < numBoards) {
 				int[][] currentBoard = queue.remove();
@@ -166,32 +166,33 @@ public class Sudoku {
 				if (nextEmptyCell == null) {
 					continue; // Skip if no empty cells are left without a solution
 				}
-		
+
 				// Try all possible values in this cell
 				for (int num = 1; num <= blockSize * blockSize; num++) {
 					if (isValidMove(currentBoard, nextEmptyCell, num)) {
-						//System.out.print("4");
+						// System.out.print("4");
 						int[][] newBoard = deepCopy(currentBoard);
 						newBoard[nextEmptyCell.x][nextEmptyCell.y] = num;
-		
+
 						// Add new state to the BFS queue
 						queue.add(newBoard);
-		
+
 						// If reached BFS depth, add to starting boards
 						if (calculateEmptyCells(newBoard) <= BFS_DEPTH) {
 							boards.add(newBoard);
-							if (boards.size() >= numBoards) break;
+							if (boards.size() >= numBoards)
+								break;
 						}
 					}
 				}
 			}
 		}
-		
+
 		return boards;
 	}
 
-	//used for printing the input/output
-	public static void printSudokuBoard(int boardSize, int blocksize, int vals[][] ) {
+	// used for printing the input/output
+	public static void printSudokuBoard(int boardSize, int blocksize, int vals[][]) {
 		for (int y = 0; y < boardSize; y++) {
 			if (y % blocksize == 0) {
 				System.out.println(" ");
@@ -208,10 +209,12 @@ public class Sudoku {
 		System.out.println("");
 	}
 
-	//prints the file
+	// prints the file
 	public static void printSudokuBoardInFile(int boardSize, int blocksize, int vals[][], int threads) {
 		try {
-			FileWriter writer = new FileWriter("./output/" +threads + "-threads" + boardSize+"x"+boardSize+"_out.txt"); // Open file for writing
+			FileWriter writer = new FileWriter(
+					"./output/" + threads + "-threads" + boardSize + "x" + boardSize + "_out.txt"); // Open file for
+																									// writing
 			writer.write("Time taken: " + (endTime - startTime) + " milliseconds\n"); // Write time taken to solve
 			writer.write("Solved Sudoku:\n");
 			for (int y = 0; y < boardSize; y++) {
@@ -232,8 +235,8 @@ public class Sudoku {
 		}
 	}
 
-	//allows a copy without accessing the same information
-	//used in the generateStartingBoards
+	// allows a copy without accessing the same information
+	// used in the generateStartingBoards
 	private static int[][] deepCopy(int[][] original) {
 		if (original == null) {
 			return null;
@@ -245,8 +248,8 @@ public class Sudoku {
 		return result;
 	}
 
-	//controls the threads and what shuts down and not
-	private static void shutdownAndAwaitTermination(ExecutorService pool){
+	// controls the threads and what shuts down and not
+	private static void shutdownAndAwaitTermination(ExecutorService pool) {
 		pool.shutdown(); // Disable new tasks from being submitted
 		try {
 			// Wait a while for existing tasks to terminate
@@ -264,32 +267,43 @@ public class Sudoku {
 		}
 	}
 
-	//this just for the input
-	private static int parseNumThreads(String input){
+	// this just for the input
+	private static int parseNumThreads(String input) {
 		try {
-            int numThreads = Integer.parseInt(input);
-            if (numThreads == 1 || numThreads == 2 || numThreads == 4 || numThreads == 8) {
-                return numThreads;
-            }
-        } catch (NumberFormatException ex) {
-            System.out.println("Number format exception for input string: " + input);
-        }
-        return -1; // Indicate invalid input
-    }
-	
+			int numThreads = Integer.parseInt(input);
+			if (numThreads == 1 || numThreads == 2 || numThreads == 4 || numThreads == 8) {
+				return numThreads;
+			}
+		} catch (NumberFormatException ex) {
+			System.out.println("Number format exception for input string: " + input);
+		}
+		return -1; // Indicate invalid input
+	}
+
+	// temporary function to test the input I get:
+	private static void printBoard(int[][] board) {
+		System.out.println("Board Layout:");
+		for (int[] row : board) {
+			for (int cell : row) {
+				System.out.print(cell + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+	}
+
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		if(args.length < 2) { //error for less than 1 args
+		if (args.length < 2) { // error for less than 1 args
 			System.out.println("Usage: java Sudoku <FILE_NAME> <NUM_THREADS>");
-            return;
+			return;
 		}
 
 		String filename = "../data/" + args[0];
 		int numThreads = parseNumThreads(args[1]);
 
-
-		if (numThreads == -1){
+		if (numThreads == -1) {
 			System.out.println("Invalid number of threads. Please use 1, 2, 4, or 8.");
-            return;
+			return;
 		}
 
 		File inputFile = new File(filename);
@@ -336,23 +350,23 @@ public class Sudoku {
 		if (count != boardSize * boardSize)
 			throw new RuntimeException("Incorrect number of inputs.");
 
-		
 		System.out.println("\nSudoku Board:");
-		printSudokuBoard(boardSize, partitionSize,vals);
+		printSudokuBoard(boardSize, partitionSize, vals);
 		Sudoku.startTime = System.currentTimeMillis(); // Record start time; code starts here
 
-		
 		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 		AtomicBoolean solutionFound = new AtomicBoolean(false);
 
 		List<int[][]> startingBoards = generateStartingBoards(vals, partitionSize, numThreads);
 		for (int[][] board : startingBoards) {
+			System.out.println("Submitting board to thread:");
+			printBoard(board);
 			executor.submit(() -> {
 				ExactMatrix matrix = new ExactMatrix(board);
 				Cell[][] finalMatrix = matrix.makeFinalMatrix();
 				DancingLinkSolver solver = new DancingLinkSolver(finalMatrix, boardSize, solutionFound, solutionMatrix);
-            });
-        }
+			});
+		}
 
 		shutdownAndAwaitTermination(executor);
 
