ce6573c0c6002dc6ea49bfa6b2109365a8aab072
sol <soleilcordray@gmail.com>
2024-04-16 10:55:29 -0400

    working but slightly slower
    parallelization

--- propagation-and-search/PossibleGrid.cpp
+++ propagation-and-search/PossibleGrid.cpp
@@ -83,28 +83,75 @@ vector<pair<Position, int>> PossibleGrid::crossReference() const
 	return pairs;
 }
 
+void workerFunction(const vector<vector<vector<int>>> &possibleValues, int index, bool isRow, int start, int end,
+					map<int, vector<Position>> &localMap, mutex &mtx)
+{
+	for (int i = start; i < end; ++i)
+	{
+		vector<int> localValues; // Local storage for thread-safe operation
+		{
+			lock_guard<mutex> lock(mtx); // Protect reading from shared vector
+			if (index < possibleValues[i].size() && isRow
+					? index < possibleValues.size() && i < possibleValues[index].size()
+					: i < possibleValues.size() && index < possibleValues[i].size())
+			{
+				const vector<int> &values = isRow ? possibleValues[index][i] : possibleValues[i][index];
+				localValues = values; // Make a local copy to work with
+			}
+			else
+			{
+				continue; // Skip this iteration if indices are out of range
+			}
+		} // Mutex unlocks here
+
+		for (int val : localValues)
+		{
+			Position pos = isRow ? Position(index, i) : Position(i, index);
+			lock_guard<mutex> lock(mtx); // Thread-safe insertion into map
+			localMap[val].push_back(pos);
+		}
+	}
+}
+
 // Identify unique values in a specified row/column
 vector<pair<Position, int>> PossibleGrid::identifyUniqueValues(int index, bool isRow) const
 {
-	multimap<int, Position> valueMap; // map: quick lookup
-	for (int i = 0; i < gridSize; ++i)
+	vector<thread> threads;
+	vector<map<int, vector<Position>>> threadMaps(numThreads);
+	mutex mtx;
+
+	// Divide work among threads
+	threadTasks = calculateThreadTasks(static_cast<size_t>(gridSize));
+	for (int i = 0; i < numThreads; ++i)
 	{
-		const vector<int> &values = isRow ? possibleValues[index][i] : possibleValues[i][index];
-		for (int val : values)
+		int start = i * threadTasks;
+		int end = (i == numThreads - 1) ? gridSize : start + threadTasks;
+		threads.emplace_back(workerFunction, ref(possibleValues), index, isRow, start, end, ref(threadMaps[i]), ref(mtx));
+	}
+
+	// Wait for all threads to complete
+	for (auto &th : threads)
+	{
+		th.join();
+	}
+
+	// Merge results
+	map<int, vector<Position>> mergedMap;
+	for (auto &tm : threadMaps)
+	{
+		for (auto &pair : tm)
 		{
-			valueMap.emplace(val, isRow ? Position(index, i) : Position(i, index));
+			mergedMap[pair.first].insert(mergedMap[pair.first].end(), pair.second.begin(), pair.second.end());
 		}
 	}
 
+	// Extract unique values
 	vector<pair<Position, int>> uniquePairs;
-	for (auto it = valueMap.begin(); it != valueMap.end(); ++it)
-	{									   // auto = type-safe
-		const auto &value = it->first;	   // key
-		const auto &position = it->second; // value
-
-		if (valueMap.count(value) == 1)
-		{ // appears once (unique)
-			uniquePairs.push_back(make_pair(position, value));
+	for (auto &pair : mergedMap)
+	{
+		if (pair.second.size() == 1)
+		{
+			uniquePairs.emplace_back(pair.second.front(), pair.first);
 		}
 	}
 
